74e458f554d59121da022fb39fdcbf1032b5a8c8
Razvan-Ungureanu <Razvan.Ungureanu@cognizant.com>
2026-03-02 17:40:00 +0200

    Add examples for logical, bitwise and special operators

---
+++ operators_examples.py
@@ -0,0 +1,17 @@
+# Logical operators
+print(True and False)
+print(True or False)
+print(not True)
+
+# Bitwise operators
+print(5 & 3)
+print(5 | 3)
+print(5 ^ 3)
+print(5 & 3)
+print(5 << 3)
+print(5 >> 3)
+
+# Special operators
+a = 5
+print(a is 5)
+print(a in [1, 2, 5])
