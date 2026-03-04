12d407cddf7fb8a3bdcec9b82f89f5db4740c2d3
Razvan-Ungureanu <Razvan.Ungureanu@cognizant.com>
2026-03-04 15:46:48 +0200

    Add most frequent word

---
+++ homework4/practice5.py
@@ -0,0 +1,24 @@
+def most_frequent_word(text):
+    words = text.split()
+    word_count = {}
+
+    for word in words:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+    
+    max_word = None
+    max_count = 0
+
+    for word in word_count:
+        if(word_count[word] > max_count):
+            max_count = word_count[word]
+            max_word = word
+
+    print("Most frequent word:", max_word)
+    print("Count:", max_count)
+
+if __name__ == "__main__":
+    text = "apple banana apple orange banana apple"
+    most_frequent_word(text)
